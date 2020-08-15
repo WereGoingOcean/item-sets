a5828a7289bf227b5d73aac58aae4c488e650115
WereGoingOcean <WereGoing@Ocean.com>
2020-08-15 14:10:45 -0400

    Adding Vue 3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>My Event</h1>
+    <p>Capacity: {{ capacity }}</p>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const capacity = ref(3);
+
+    function increaseCapacity() {
+      capacity.value++;
+    }
+    return { capacity, increaseCapacity };
+  },
+};
+</script>
---
+++ src/vue-main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App";
+
+console.log("custom code");
+
+createApp(App).mount("#vue-app");
