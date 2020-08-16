4bdac05f10a936f3ed12121b1159617747d22db9
Dylan <steelersfanmw2@gmail.com>
2020-08-16 14:22:18 -0400

    Super simple electron by itself

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const { app, BroswerWindow, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Create a browser window
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load an index.html
+  win.loadFile("index.html");
+}
+
+app
+  .whenReady()
+  .then(() => {
+    console.log("App ready");
+
+    createWindow();
+  })
+  .catch((err) => {
+    console.log(`Error creating window ${err}`);
+    app.quit();
+  });
